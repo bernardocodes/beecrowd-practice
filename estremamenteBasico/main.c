f4455832b30bccf2ef389b57b57bb0a514fe39f0
Bernardo <121183049+bernardocodes@users.noreply.github.com>
2024-09-08 14:09:06 -0300

    Create main.c
    100%

---
+++ estremamenteBasico/main.c
@@ -0,0 +1,10 @@
+//https://judge.beecrowd.com/pt/problems/view/1001
+#include <stdio.h>
+
+int main(void) {
+  int a,b,c;
+  scanf("%d%d",&a,&b);
+  c = a + b;
+  printf("SOMA = %d\n",c);
+  return 0;
+}
