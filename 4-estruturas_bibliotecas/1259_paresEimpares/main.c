fe3eac71bdbfe29f93f900326da10aae9b46e2fa
Bernardo <bernardo.polaki@gmail.com>
2024-09-13 22:04:38 -0300

    Update main.c
    Implementei a separação entre par e impar, posicionando os números pares a esquerda e os impares a direita.

--- 4-estruturas_bibliotecas/1259_paresEimpares/main.c
+++ 4-estruturas_bibliotecas/1259_paresEimpares/main.c
@@ -10,7 +10,7 @@ int parOuImpar(int a){//impar???
 	}
 }
 
-void printaVet(int vet[],int tam){
+void printaVet(int vet[],int tam){//imprime vet
 	for(int i=0;i<tam;i++){
 		printf("%d ",vet[i]);
 	}
@@ -20,11 +20,16 @@ int ordenaVet(){
 	
 }
 
-int separaParImpar(int vet,int tam){
+void separaParImpar(int *vet,int tam){
 	int aux;
+	int p1 = 0;
 	for(int i=0;i<tam;i++){//percorrer o vetor
-		if(parOuImpar(vet[i])==0){
-			aux = vet[i];
+		if(parOuImpar(vet[i])==0){//se for par, coloca na primeira parte do vetor
+			aux = vet[p1];
+			vet[p1] = vet[i];
+			vet[i] = aux;
+			p1++;
+		}
 			
 	}
 }
@@ -38,7 +43,8 @@ int main(){
 	*/
 
 	int tam = 10;
-	int vet[tam] = {0,1,2,3,4,5,6,7,8,9};//tem que ficar 0,2,4,6,8,1,3,5,7,9
+	int vet[] = {0,1,2,3,4,5,6,7,8,9};//tem que ficar 0,2,4,6,8,1,3,5,7,9
+	separaParImpar(vet,tam);//AQ
 	printaVet(vet,tam);
 
 	return 0;
