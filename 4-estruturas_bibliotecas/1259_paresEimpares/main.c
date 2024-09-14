591de1aedfcf5e2ac07dbee57a25dedd22fad341
Bernardo <bernardo.polaki@gmail.com>
2024-09-14 12:42:49 -0300

    teste

--- 4-estruturas_bibliotecas/1259_paresEimpares/main.c
+++ 4-estruturas_bibliotecas/1259_paresEimpares/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int contPares = 0;
+
 int parOuImpar(int a){//impar???
 	if(a%2==0){
 		//printf("PAR\n");
@@ -16,8 +18,18 @@ void printaVet(int vet[],int tam){//imprime vet
 	}
 }
 
-int ordenaVet(){
-
+void ordenaVet(int *vet, int tam){//ORDENAÇÃO DA ESQUERDA PARA A DIREITA
+	int menor = 0;//posição analisada
+	int aux;	
+	for(int i=0;i<tam-1;i++){//pares
+		for(int j=i;j<tam;j++){
+			if(vet[menor] < vet[j]){
+				aux = vet[menor];
+				vet[menor] = vet[j];
+				vet[j] = aux;
+			}
+		}
+	}
 }
 
 void separaParImpar(int *vet,int tam){//PARES A ESQUERDA
@@ -29,20 +41,17 @@ void separaParImpar(int *vet,int tam){//PARES A ESQUERDA
 			vet[p1] = vet[i];
 			vet[i] = aux;
 			p1++;
+			contPares++;
 		}	
 	}
 }
 
 int main(){
 
-	/*
-	   int A = 0;
-	   scanf("%d",&A);
-	   parOuImpar(A);
-	   */
-
 	int tam = 10;
-	int vet[] = {0,1,2,3,4,5,6,7,8,9};//tem que ficar 0,2,4,6,8,1,3,5,7,9
+	int vet[] = {0,7,2,8,4,5,9,1,3,6};//tem que ficar 0,2,4,6,8,1,3,5,7,9
+
+	ordenaVet(vet,tam);
 	separaParImpar(vet,tam);//AQ
 	printaVet(vet,tam);
 
